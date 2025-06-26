3662a48344839d2475ae1a4676bfaee8ddc6697d
aman <amankumarkanu87@gmail.com>
2025-06-26 12:27:24 +0530

    this is the second file

---
+++ variabls.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#this is the single line comment
+
+ : << 'comment'
+
+this is the multi line comment we can write any things inside this comment 
+
+comment
+
+#variables..
+
+name="aman"
+echo "the name is : $name"
